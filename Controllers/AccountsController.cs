ae3ca33a1e7a59dfb23ff134cccf9d1aaccf9d4b
khoaNN <khoann1999@gmail.com>
2020-07-19 21:40:20 +0700

    no message

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -20,84 +20,6 @@ namespace MyStudioWebApi.Controllers
             _context = context;
         }
 
-        // GET: api/Accounts
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Account>>> GetAccount()
-        {
-            return await _context.Account.ToListAsync();
-        }
-
-        // GET: api/Accounts/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Account>> GetAccount(string id)
-        {
-            var account = await _context.Account.FindAsync(id);
-
-            if (account == null)
-            {
-                return NotFound();
-            }
-
-            return account;
-        }
-
-        // PUT: api/Accounts/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAccount(string id, Account account)
-        {
-            if (id != account.UserName)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(account).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AccountExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Accounts
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost("Account")]
-        public async Task<ActionResult<Account>> PostAccount(Account account)
-        {
-            _context.Account.Add(account);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (AccountExists(account.UserName))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return CreatedAtAction("GetAccount", new { id = account.UserName }, account);
-        }
 
         // POST: api/Accounts
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
@@ -115,25 +37,5 @@ namespace MyStudioWebApi.Controllers
             return account;
         }
 
-        // DELETE: api/Accounts/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Account>> DeleteAccount(string id)
-        {
-            var account = await _context.Account.FindAsync(id);
-            if (account == null)
-            {
-                return NotFound();
-            }
-
-            _context.Account.Remove(account);
-            await _context.SaveChangesAsync();
-
-            return account;
-        }
-
-        private bool AccountExists(string id)
-        {
-            return _context.Account.Any(e => e.UserName == id);
-        }
     }
 }
